1cbaab1b0700ea955ae5c662390cece2b11f822c
FleurLis <53279500+DungeonCR@users.noreply.github.com>
2021-06-03 01:57:59 +0800

    .net seems better

--- nhentai.js
+++ nhentai.js
@@ -2,5 +2,5 @@ let btnOpen = document.querySelector('button');
 let input = document.querySelector('input');
 
 btnOpen.addEventListener('click', () => {
-    window.open('https://nhentai.to/g/' + input.value, '_black');
+    window.open('https://nhentai.net/g/' + input.value, '_black');
 });
