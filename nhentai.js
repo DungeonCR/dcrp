898c3cc3a556771e84c562d805fd714eced43512
DungeonCR <dungeoncr@icloud.com>
2021-06-24 19:15:41 +0800

    nhentai works much better

--- nhentai.js
+++ nhentai.js
@@ -1,13 +1,7 @@
-
-let btnOpen = document.querySelector('button');
 let input = document.querySelector('input');
 
-btnOpen.addEventListener('click', () => {
-    window.open('https://nhentai.net/g/' + input.value, '_black');
-});
-
-document.getElementById("demo").addEventListener("click", myFunction);
-
-function myFunction() {
-    window.open('https://nhentai.net/g/' + input.value, '_black');
-}
+input.addEventListener('keyup', (e) => {
+    if(e.keyCode === 13){
+        window.open('https://nhentai.net/g/' + e.target.value, '_blank');
+    }
+})
